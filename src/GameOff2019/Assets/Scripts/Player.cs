ec71e25f4e345bea69e8cb6d1ea20651fcb625de
Astro Pickle <astropickle@chriscampbell.codes>
2019-11-05 22:12:17 +1100

    Add very simple run physics

--- src/GameOff2019/Assets/Scripts/Player.cs
+++ src/GameOff2019/Assets/Scripts/Player.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float fullJumpHeightInUnits = 1.8f;
+    public float baseRunVelocity = 2f;
+    public float fallGravityMultiplier = 2.5f;
+    public float lowJumpGravityMultiplier = 2f;
+
     private Rigidbody2D rb2d;
     private BoxCollider2D boxCollider;
 
+    private bool jumpButtonIsPressed = false;
     private bool isJumping = false;
-    private float jumpVelocity = 15f;
-    private float fallGravityMultiplier = 2.5f;
-    private float lowJumpMultiplier = 2f;
+    private float jumpVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+
+        // TODO: Do fancy maths to caltulate the exact required velocity to jump X units.
+        jumpVelocity = fullJumpHeightInUnits;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool jumpButtonIsPressed = false;
-
         if (Input.GetKey("space"))
         {
             jumpButtonIsPressed = true;
+        }
+        else
+        {
+            jumpButtonIsPressed = false;
         }
 
         // Check if the player needs to jump
@@ -44,13 +53,14 @@ public class Player : MonoBehaviour
         // Check if the player is moving upwards
         else if (rb2d.velocity.y > 0 && jumpButtonIsPressed == false)
         {
-            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+            rb2d.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpGravityMultiplier - 1) * Time.deltaTime;
         }
     }
 
     void FixedUpdate()
     {
-
+        // TODO: Replace this run with something better
+        rb2d.velocity = new Vector2(baseRunVelocity, rb2d.velocity.y);
     }
 
     private void jump()
