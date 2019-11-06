ae614829efd79e7e01c1153135cdc47ed8d8bec6
Astro Pickle <astropickle@chriscampbell.codes>
2019-11-07 08:27:15 +1100

    Replace old game with new space ship game

---
+++ src/GameOff2019/Assets/Scripts/ShipController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+
+    public float thrusterForce = 2f;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddForceAtPosition(transform.up, transform.position - new Vector3(-1, -1, 0));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddForceAtPosition(transform.up, transform.position - new Vector3(1, -1, 0));
+        }
+    }
+}
