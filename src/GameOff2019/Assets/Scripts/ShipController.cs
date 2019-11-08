3d76a30505bb28f3eab213144eefbbd74aba41a2
Astro Pickle <astropickle@chriscampbell.codes>
2019-11-08 16:31:53 +1100

    Start to add logic for ship looping to other sides of the screen

--- src/GameOff2019/Assets/Scripts/ShipController.cs
+++ src/GameOff2019/Assets/Scripts/ShipController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +9,13 @@ public class ShipController : MonoBehaviour
     public float thrusterForce = 2f;
 
     private Rigidbody2D rb2d;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,5 +30,35 @@ public class ShipController : MonoBehaviour
         {
             rb2d.AddForceAtPosition(transform.up, transform.position - new Vector3(1, -1, 0));
         }
+    }
+
+    void FixedUpdate()
+    {
+        // Check if player has gone off the screen 
+        //// Top
+        //if (transform.position.y > 12f)
+        //{
+        //    transform.position = new Vector3(transform.position.x, (-transform.position.y) - spriteRenderer.bounds.size.y);
+        //}
+        //// Bottom
+        //else if (transform.position.y < -12f)
+        //{
+        //    transform.position = new Vector3(transform.position.x, (-transform.position.y) + spriteRenderer.bounds.size.y);
+        //}
+        //// Left
+        //else if (transform.position.x < -20f)
+        //{
+        //    transform.position = new Vector3(-transform.position.x + spriteRenderer.bounds.size.x, transform.position.y);
+        //}
+        //// Right
+        //else if (transform.position.x > 20f)
+        //{
+        //    transform.position = new Vector3(-transform.position.x - spriteRenderer.bounds.size.x, transform.position.y);
+        //}
+
+        //if (transform.position.y > 12f || transform.position.y < -12f)
+        //{
+        //    transform.position = new Vector3(transform.position.x, (-transform.position.y) + -(Math.Sign(transform.position.y) * (spriteRenderer.bounds.size.y)));
+        //}
     }
 }
