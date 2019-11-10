f448d9e66f3b4f36789aa43a7aab9b6f4ddf2911
Astro Pickle <astropickle@chriscampbell.codes>
2019-11-10 18:21:15 +1100

    Add basic version of background parallax

---
+++ src/GameOff2019/Assets/Scripts/BackgroundParallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour
+{
+    public GameObject ship;
+    public float parallaxMultiplier = 0.01f;
+
+    private Renderer quadRenderer;
+    private Rigidbody2D shipRb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quadRenderer = GetComponent<Renderer>();
+        shipRb2d = ship.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        quadRenderer.material.mainTextureOffset = new Vector2(shipRb2d.velocity.x * parallaxMultiplier, shipRb2d.velocity.y * parallaxMultiplier);
+    }
+}
